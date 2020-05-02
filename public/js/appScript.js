1ce44986432b952678441ab5a721611bf4631589
Navin Shekhar <nshekhar@BLR.ALLEGISINDIA.COM>
2020-05-02 10:21:46 +0530

    adding cor-anywhere

--- public/js/appScript.js
+++ public/js/appScript.js
@@ -11,7 +11,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('https://cors-anywhere.herokuapp.com/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 messageOne.textContent = data.error
