19a96bb49892829b21ed902e819b7eec581648a2
Navin Shekhar <nshekhar@BLR.ALLEGISINDIA.COM>
2020-05-02 10:00:36 +0530

    Changes before moving to server

--- src/app.js
+++ src/app.js
@@ -7,10 +7,12 @@ const express = require('express')
 const hbs = require('hbs')
 
 const app = express()
+
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials/')
 
+let port = process.env.PORT || 3000;
 
 // Define paths for Engine config
 app.set('view engine', 'hbs');
@@ -109,6 +111,6 @@ app.get('*', (req, res)=>{
     })
 });
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log('Server is up on port:' + port)
 })
